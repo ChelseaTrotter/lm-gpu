ddb36c5cf1ddfad61e4a26656495f5ebb6e56493
Xiaoqi Hu <xiaoqihu@bitbucket.org>
2019-06-13 14:49:36 -0500

    simple reduction find max kernel

--- src/gpu.jl
+++ src/gpu.jl
@@ -1,3 +1,4 @@
+import CuArrays.CuArray
 
 function calculate_r(a::CuArray,b::CuArray)
     return CuArrays.BLAS.gemm('T', 'N', a,b);
@@ -8,53 +9,68 @@ function gpurun(a::Array{Float64,2}, b::Array{Float64,2},n,m,p)
     b_std = get_standardized_matrix(b);
     d_a = CuArray(a_std);
     d_b = CuArray(b_std);
-    d_output = CuArray(zeros(Float64, 1, p))
     d_r = calculate_r(d_a,d_b);
-    gpu_square_lod(d_r,n)
-    gpu_reduce(d_r,d_output, m,p)
-    return collect(d_output)
+    gpu_square_lod(d_r,n,m,p)
+    # gpu_reduce(d_r,m,p)
+    return collect(d_r[1, 1:p])
 end
 
-function gpu_square_lod(d_r::CuArray{Float64,2},n)
+function gpu_square_lod(d_r::CuArray{Float64,2},n,m,p)
     #Get total number of threads 
     ndrange = prod(size(d_r))
     #Get maximum number of threads per block
     dev = device()
     threads = attribute(dev, CUDAdrv.WARP_SIZE)
     blocks = min(Int(ceil(ndrange/threads)), attribute(dev, CUDAdrv.MAX_GRID_DIM_X))
-    return @cuda blocks=blocks threads=threads lod_kernel(d_r, ndrange,n)
+    @cuda blocks=blocks threads=threads lod_kernel(d_r, ndrange,n)
+    return @cuda blocks=blocks threads=threads reduce_kernel(d_r,m,p)
     
 end
 
 function lod_kernel(input, MAX,n) 
-    i = (blockIdx().x-1) * blockDim().x + threadIdx().x
-    if(i < MAX+1)
+    tid = (blockIdx().x-1) * blockDim().x + threadIdx().x
+    if(tid < MAX+1)
         # r_square = (a[i] * a[i]) / (n*n)
-        r_square = (input[i]/n)^2
-        input[i] = (-n/Float64(2.0)) * CUDAnative.log(Float64(1.0)-r_square)
+        r_square = (input[tid]/n)^2
+        input[tid] = (-n/Float64(2.0)) * CUDAnative.log(Float64(1.0)-r_square)
     end 
-    return 
+    return
 end
 
-function reduce_kernel(input, output, ndrange, rows)
-    i = (blockIdx().x-1) * blockDim().x + threadIdx().x
-    if(i < ndrange+1)
-        for r in 1:rows
-            output[i] += input[r]
+function reduce_kernel(input, rows, cols)
+    tid = (blockIdx().x-1) * blockDim().x + threadIdx().x
+    # Trying for shared memory:
+    # shmem = @cuDynamicSharedMem(Float64, rows)
+    # for i in 1:rows
+    #     shmem[i] = input[i, tid]
+    # end
+    # temp_max = shmem[1]
+
+    # Trying for simplest kernel
+    if(tid < cols)
+        temp_max = input[1, tid]
+        for i in 1:rows
+            temp_max = max(temp_max,input[i, tid])
         end
+        input[1, tid] = temp_max
     end
+    return
 end
 
-
-function gpu_reduce(input::CuArray{Float64,2},output::CuArray{Float64,2}, rows, cols)
+function gpu_reduce(input::CuArray{Float64,2}, rows, cols)
     #Get total number of threads 
-    ndrange = cols
+    # ndrange = cols
     #Get maximum number of threads per block
-    dev = device()
-    threads = attribute(dev, CUDAdrv.WARP_SIZE)
+    # dev = device()
+    # threads = attribute(dev, CUDAdrv.WARP_SIZE)
     # result = CuArray(zeros(m,p))
     # blocks = min(Int(ceil(ndrange/threads)), attribute(dev, CUDAdrv.MAX_GRID_DIM_X))
-    blocks = Int(ceil(ndrange/threads))
-    return @cuda blocks=blocks threads=threads reduce_kernel(input,output,ndrange,rows)
+    # blocks = Int(ceil(ndrange/threads))
+
+    # Trying for shared memory. 
+    # shmem = cols * sizeof(Float64)
+    # return @cuda blocks=1 threads=cols shmem=shmem reduce_kernel(input,rows,cols)
+
+    return @cuda blocks=1 threads=cols reduce_kernel(input,rows,cols)
      
 end
